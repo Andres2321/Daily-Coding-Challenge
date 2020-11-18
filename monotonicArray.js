28a8af6209e5cffaa35c3b42be2c18b9389975a4
Andres <andres-a23@live.com>
2020-11-17 21:49:23 -0500

    monotonic array challenge

--- monotonicArray.js
+++ monotonicArray.js
@@ -5,13 +5,27 @@
 // increasing; they simply don't decrease. Note that empty arrays and arrays of one element are monotonic.
 
 // ----- Pseudocode -----
-// 
-// 
-// 
-// 
+// Iterate through array with a while loop
+// define variable for starting point
+// define the next variable
+// compare them and find their direction
 
+// SOLUTION 1
 const isMonotonic = (array) => {
+    // Set variable to non-decreasing as true
+    let isNonDecreasing = true
+    // Set variable to non-increasing as true
+    let isNonIncreasing = true
 
+    // Iterate through the array
+    for(let i = 1; i < array.length; i++){
+        // If index is less than the next index - array is increasing so make isNonDecreasing as false
+        if(array[i] < array[i - 1]) isNonDecreasing = false
+        // If index is greater than the next index - array is decreasing so make isNonIncreasing as false
+        if(array[i] > array[i - 1]) isNonIncreasing = false
+    }
+    // return whatever variable is true
+    return isNonDecreasing || isNonIncreasing
 }
 
 const array = [-1,-5,-10,-1100,-1100,-1101,-1102,-9001]
