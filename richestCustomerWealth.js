efdaeb2522a80f00d048878317d46e729e8155c1
Andres <andres-a23@live.com>
2020-11-29 23:35:21 -0500

    wealthiest customer

---
+++ richestCustomerWealth.js
@@ -0,0 +1,3 @@
+// You are givenan m x n integer grid  accounts[i][j] is the amount of the ith customer has in the jth bank. Return
+// the wealth the the richest customer has. A customer's wealth si the amount of money they have in all their bank account.
+// The richest customer is the customer that has the maximum wealth.
